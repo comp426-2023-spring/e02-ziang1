0130166dcd30240b064f4700b4ae58d229af4cd1
Jeremy Chen <ziang1@email.unc.edu>
2023-05-09 17:10:23 -0400

    update

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import {rps, rpsls} from './lib/rpsls.js';
+
 //// Load most basic dependencies
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
@@ -63,6 +65,57 @@ const app = express()
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
 //
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// Check endpoint @ /app
+app.get('/app', (req, res) => {
+    res.status(200).send("200 OK");
+})
+
+// check endpoint at /app/rps
+app.get('/app/rps/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));
+})
+
+// /app/rpsls endpoint
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));
+})
+
+// /app/rps/play endpoint (bodies & params)
+app.get('/app/rps/play/', (req, res) => { //rps URLEncoded
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+})
+
+app.post('/app/rps/play/', (req, res) => { //rps JSON
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+})
+
+app.get('/app/rps/play/:shot', (req, res) => { //rps shot params
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+})
+
+// /app/rpsls/play endpoint
+app.get('/app/rpsls/play/', (req, res) => { //rpsls URLEncoded
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+})
+
+app.post('/app/rpsls/play/', (req, res) => { //rpsls JSON
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => { //rpsls shot params
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+})
+
+
+// default endpoint)
+app.get("/app/*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");
+})
+
 // Create and update access log
 // The morgan format below is the Apache Foundation combined format but with ISO8601 dates
 app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
